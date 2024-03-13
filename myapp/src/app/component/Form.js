cfa1d5aad2bafc245ad80f7b4048a134947eb3a5
Hyeonqz <wlsgusrb78@naver.com>
2024-03-13 18:23:43 +0900

    [Todo] : react-hook(useRef)

--- myapp/src/app/component/Form.js
+++ myapp/src/app/component/Form.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
 const Form = (props) => {
     const handleSubmit = (e) => {
         e.preventDefault();
+        props.insertUser({
+            name : e.target.name.value,
+            address : e.target.address.value,
+            email: e.target.email.value
+        });
         // 입력받은 데이터를 처리한다.
         e.target.reset();
+    }
+
+    const nameRef = useRef();
+    const emailRef = useRef();
+
+    const handleChangeName = () => {
+        setName(nameRef.current.value);
+    };
+
+    const handleChangeEmail = () => {
+        setEmail(emailRef.current.value);
     }
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor='name'>이름 : </label>
-                    <input id='name' />
+                    <input ref={nameRef} value={name} onChange={handleChangeName} />
                 </div>
                 <div>
                     <label htmlFor='address'>주소 : </label>
-                    <input id='address' />
+                    <input ref={emailRef} value={email} onChange={handleChangeEmail}  />
                 </div>
                 <div>
                     <label htmlFor='email'>이메일 : </label>
--- myapp/src/app/component/List.js
+++ myapp/src/app/component/List.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Form from "@/app/component/Form";
 
 const List = () => {
+    const[users, setUsers] = useState([]);
+    const insertUser = (user) => {
+        setUsers(prevUsers => [...prevUsers, user]);
+    }
     return (
         <div>
-            
+            <Form insertUser={insertUser} />
         </div>
     );
 };
