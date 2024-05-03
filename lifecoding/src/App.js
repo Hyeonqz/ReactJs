c20fb92e18416f51e590d4f23ebf7608191e185a
Hyeonqz <wlsgusrb78@naver.com>
2024-05-03 14:14:20 +0900

    생활코딩 리액트 #2

--- lifecoding/src/App.js
+++ lifecoding/src/App.js
@@ -1,33 +1,87 @@
 import './App.css';
+import {useState} from 'react';
 
-
-function Header() {
-  return <h1><a href="/">React</a></h1>;
-}
-
-function Nav() {
+function Nav(props) {
+  const list = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    list.push(
+      <li key={t.id}>
+        <a href={'/read/' + t.id} onClick={(event)=> {
+          event.preventDefault();
+        }}>
+          {t.title}
+        </a>
+      </li>);
+  }
   return <nav>
     <ol>
-      <li><a href="/read/1">html</a></li>
-      <li><a href="/read/2">css</a></li>
-      <li><a href="/read/3">js</a></li>
+      {list}
     </ol>
-  </nav>
+  </nav>;
 }
 
-function Article() {
+function Article(props) {
   return <article>
-    <h2>Welcome</h2>
-    Hello, React?
+    <h2>{props.title}</h2>
+    {props.body}
   </article>;
 }
 
+function Header(props) {
+  return <header>
+    <h1>
+      <a href="/" onClick={function (event) {
+          event.preventDefault(); // 이벤트 방지하기
+      }}>
+        {props.title}React
+      </a>
+    </h1>
+  </header>;
+}
+
 function App() {
+  console.log(_mode);
+  const _mode = useState('WELCOME');
+  const topics = [
+    {
+      id: 1,
+      title: 'html',
+      body: 'html is ...',
+    },
+    {
+      id: 2,
+      title: 'css',
+      body: 'css is ...',
+    },
+    {
+      id: 3,
+      title: 'js',
+      body: 'js is ...',
+    },
+    {
+      id: 4,
+      title: 'react',
+      body: 'react is ...',
+    }
+  ];
+  let content = null;
+
+  if(mode === 'WELCOME') {
+    content = <Article title="Welcome" body="Hello, WEB"></Article>
+  } else if(mode === 'READ') {
+    <Article title="Read" body="Hello, Read"></Article>
+  }
   return (
     <div className="App">
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title="WEB" onChangeMode={()=> {
+        mode = 'WELCOME';
+      }}>
+      </Header>
+      <Nav topics={topics} onchangeMode={(id)=>{
+        mode = 'READ';
+      }}></Nav>
+      {content}
     </div>
   );
 }
---
+++ lifecoding/src/Hook.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+
+
+
+
+
+function Hook(props) {
+  const mode = 'WELCOME';
+  const topics = [
+    {
+      id: 1,
+      title: 'html',
+      body: 'html is ...',
+    },
+    {
+      id: 2,
+      title: 'css',
+      body: 'css is ...',
+    },
+    {
+      id: 3,
+      title: 'js',
+      body: 'js is ...',
+    },
+    {
+      id: 4,
+      title: 'react',
+      body: 'react is ...',
+    }
+  ]
+  let content = null;
+
+  return (
+    <div>
+
+
+    </div>
+  );
+}
+
+export default Hook;
